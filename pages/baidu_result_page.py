78db2d2a18252a652f7e2d2a6992618be684e605
19950902402 <3032573218@qq.com>
2026-03-30 21:33:21 +0800

    feat: 忽略__pycache__缓存文件，清理冗余文件

--- pages/baidu_result_page.py
+++ pages/baidu_result_page.py
@@ -5,3 +5,11 @@ from selenium.webdriver.support import expected_conditions as EC
 class BaiduResultPage(BasePage):
 
     def wait_title_contains(self, keyword, timeout=10):
+        """只等待，不校验"""
+        wait = WebDriverWait(self.driver, timeout)
+        wait.until(EC.title_contains(keyword))
+
+    # 获取当前页面标题（给用例类使用）
+    def get_page_title(self):
+        return self.driver.title
+        return self.driver.title
